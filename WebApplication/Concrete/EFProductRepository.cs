b5f4de56b289447182f4a4c8179e1359d2d94699
GITD-2846\Leszek <lzalewski.leszek@gmail.com>
2017-01-31 12:20:13 +0100

    Projekt podstawowy z entity

---
+++ WebApplication/Concrete/EFProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication.Abstract;
+using WebApplication.Entities;
+
+namespace WebApplication.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+
+        private EFDbContext context = new EFDbContext();
+        public IQueryable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using WebApplication.Abstract;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IProductRepository repository;
+
+        public HomeController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ActionResult DashboardV1()
+        {
+            return View();
+        }
+        public ActionResult DashboardV2()
+        {
+            return View();
+        }
+
+
+       
+          
+      
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+
+    }
+}
